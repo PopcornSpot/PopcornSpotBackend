cc6a1d31383a2b755956b9b29152d2d05c72d43d
Vignesh1066 <vignesh4974@gmail.com>
2024-12-04 01:55:37 +0530

    Created image url

--- src/index.js
+++ src/index.js
@@ -11,15 +11,16 @@ const cors=require("cors")
 const app = express();
 app.use(cors("*"))
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
 connections.connect();
+app.use("/upload", express.static("src/fileStorage"));
 app.use("/auth", authRoute);
 app.use("/superadmin",superAdminRoute)
 app.use("/admin",adminRoute)
 app.use("/user",userRoute)
 app.use("/movie",movieRoute)
 
-
+//http://localhost:7000/upload/1733254662486-InShot_20240825_115055817.jpg
 
 app.use("/", (req, res) => {
     res.send("I'm alive")
